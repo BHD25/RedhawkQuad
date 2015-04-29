1b563c670503d567e30de4039711d74dcc1000cf
BHD25 <jthomas2892@gmail.com>
2015-04-29 18:20:20 -0400

    Modified for SDE display

--- Raspberry_PI/PWM/takeoff.py
+++ Raspberry_PI/PWM/takeoff.py
@@ -8,21 +8,22 @@ from threading import Thread
 
 MAX_LENGTH = 4096
 
-#isKill = 0
+isKill = 0
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(8, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(9, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#GPIO.setup(8, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#GPIO.setup(9, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+#GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 #def hasBeenKilled(channel):
 #	isKill = 1
 
 ser = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=3.0)
 ser.open()
+ser.flush()
 
 # 1 roll
 # 2 pitch
@@ -45,31 +46,49 @@ def handle(clientsocket):
 			buf = clientsocket.recv(MAX_LENGTH)
 			navigating = 0
 			#autopilot.initialize()
-			movDirection = 22
+			movDirection = 2
 			search = 0
 			serInput = ''
+			sensor1 = 0
+			sensor2 = 0
+			sensor3 = 0
+			sensor4 = 0
 			if(buf == 'takeoff'):
 				autopilot.initialize()
-				autopilot.takeOff()
+				autopilot.arm()
+				#autopilot.takeOff()
 				navigating = 1
-
+	
 			if buf == '': return
-
+	
 			while(navigating == 1):
-				print("In navigation\n")
-				serInput = ser.readline()
-				print(repr(serInput))
-				sensor1 = GPIO.input(9)
+				#print("In navigation\n")
+				if(ser.inWaiting()):
+					serInput = ser.readline()
+					#if 'f' in serInput: sensor1 = 1
+					#else: sensor1 = 0
+					#if 'r' in serInput: sensor2 = 1
+					#else: sensor2 = 0
+					#if 'b' in serInput: sensor3 = 1
+					#else: sensor3 = 0
+					#if 'l' in serInput: sensor4 = 1
+					#else: sensor4 = 0
+					#if 'k' in serInput: isKill = 1
+					#else: isKill = 0
+				#print("After Serial Read")
+				print(serInput)
+				#time.sleep(1)
+				#sensor1 = GPIO.input(9)
 				#print("Sensor1 = " + str(sensor1))
-				sensor2 = GPIO.input(7)
+				#sensor2 = GPIO.input(7)
 				#print("Sensor2 = " + str(sensor2))
-				sensor3 = GPIO.input(8)
+				#sensor3 = GPIO.input(8)
 				#print("Sensor3 = " + str(sensor3))
-				sensor4 = GPIO.input(11)
+				#sensor4 = GPIO.input(11)
 				#print("Sensor4 = " + str(sensor4))
-				#buf = clientsocket.recv(MAX_LENGTH)
+				buf = clientsocket.recv(MAX_LENGTH)
 				#if buf == '': return
-				isKill = GPIO.input(15)
+				#isKill = GPIO.input(15)
 				print(isKill)
 	
 				# Initial move right
@@ -78,13 +97,13 @@ def handle(clientsocket):
 					autopilot.strafeR()
 					autopilot.forward()
 					movDirection = 2
-					#print('Moving right\n')
+					print('Moving right')
 	
 				if(sensor3 == 0 and sensor1 == 0 and sensor4 == 0 and sensor2 == 0 and movDirection == 2):
 					autopilot.strafeR()
 					autopilot.backward()
 					movDirection = 2
-					#print('Moving right-ADJ\n')
+					print('Moving right-ADJ')
 	
 				# Found right wall, start moving forward
 				if(sensor1 == 0 and sensor2 == 1 and (sensor3 == 0 or sensor3 == 1) and sensor4 == 0 and movDirection == 2):
@@ -93,7 +112,7 @@ def handle(clientsocket):
 					time.sleep(1)
 					autopilot.forward()
 					movDirection = 1
-					#print('Moving forward\n')
+					print('Start Moving forward')
 	
 				# Following right wall
 				if(sensor1 == 0 and sensor2 == 1 and sensor3 == 0 and sensor4 == 0 and movDirection == 1):
@@ -102,14 +121,14 @@ def handle(clientsocket):
 					autopilot.forward()
 					autopilot.strafeL()
 					#movDirection = 1
-					#print('Moving forward\n')
+					print('Moving forward')
 	
 				if(sensor1 == 0 and sensor2 == 0 and sensor3 == 0 and sensor4 == 0 and movDirection == 1):
 					#autopilot.stop()
 					autopilot.forward()
 					autopilot.strafeR()
 					#movDirection = 1
-					#print('Moving forward\n')
+					print('Moving forward-ADJ')
 	
 				# Hit a right back corner
 				if(sensor1 == 1 and sensor4 == 0 and sensor2 == 1 and sensor3 == 0 and movDirection == 1):
@@ -118,7 +137,7 @@ def handle(clientsocket):
 					#time.sleep(.3)
 					autopilot.strafeL()
 					movDirection = 4
-					#print('Moving left\n')
+					print('Start Moving left')
 	
 				# Moving left following front wall
 				if(sensor1 == 1 and sensor4 == 0 and sensor2 == 0 and sensor3 == 0 and movDirection == 4):
@@ -128,7 +147,7 @@ def handle(clientsocket):
 					autopilot.strafeL()
 					autopilot.backward()
 					#movDirection = 4
-					#print('Moving left\n')
+					print('Moving left')
 	
 				if(sensor1 == 0 and sensor4 == 0 and sensor2 == 0 and sensor3 == 0 and movDirection == 4):
 					autopilot.stop()
@@ -136,7 +155,7 @@ def handle(clientsocket):
 					autopilot.strafeL()
 					autopilot.forward()
 					#movDirection = 4
-					#print('Moving left\n')
+					print('Moving left-ADJ')
 	
 				# Lost front wall
 				if(sensor1 == 0 and sensor2 == 0 and sensor3 == 0 and sensor4 == 0 and movDirection == 4):
@@ -156,11 +175,12 @@ def handle(clientsocket):
 						autopilot.strafeR()
 						time.sleep(1)
 						autopilot.stop()
-					#print('Wall vanished\n')
+					print('Wall vanished')
+					ser.flush()
 	
 				# Found back left corner
 				if(sensor1 == 1 and sensor2 == 0 and sensor3 == 0 and sensor4 == 1 and movDirection == 4):
-					#print("Found back left corner")
+					print("Found back left corner")
 					autopilot.stop()
 					time.sleep(1)
 					autopilot.turnR()
@@ -169,6 +189,7 @@ def handle(clientsocket):
 					time.sleep(1)
 					search = 1
 					navigating = 0
+					ser.flush()
 	
 				# No direction to fly
 				if(sensor1 == 1 and sensor2 == 1 and sensor3 == 1 and sensor4 == 1):
@@ -176,18 +197,21 @@ def handle(clientsocket):
 					autopilot.land()
 					time.sleep(5)
 					flying = 0
-					#print('Can\'t fly\n')
+					print('Can\'t fly')
 	
 				# Emergency kill command
 				if(buf == "kill"):
-					autopilot.land()
-					time.sleep(10)
-					autopilot.landed()
+					autopilot.throttleCut()
+					#time.sleep(10)
+					#autopilot.landed()
 					flying = 0
-					#print('Killing operation\n')
+					#print('Killing operation')
 	
 				if(isKill):
-					autopilot.throttleCut()
+					#autopilot.throttleCut()
+					autopilot.stop()
+					autopilot.land()
+					time.sleep(10)
 					flying = 0
 					return
 					#print('Killed manually')
@@ -196,11 +220,14 @@ def handle(clientsocket):
 	
 			#while(search == 1 and buf != 'stop'):
 				# Capture image and look for ball
+	except KeyboardInterrupt:
+	#	clientsocket.shutdown()
+		clientsocket.close()
+	
 	except:
 		print('Error occured, closing socket')
-		clientsocket.shutdown()
+	#	clientsocket.shutdown()
 		clientsocket.close()
-
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
